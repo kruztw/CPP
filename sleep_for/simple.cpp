7cd0a6aa0138523ac6858489d6e277edcadceb55
kruztw <kruztw@gmail.com>
2022-01-13 16:45:10 +0800

    update sleep_for

--- sleep_for/simple.cpp
+++ sleep_for/simple.cpp
@@ -2,8 +2,13 @@
 #include <chrono>
 #include <thread>
 
+using namespace std::chrono_literals; // ns us ms s min...
+
 int main()
 {
   printf("wait a second ...\n");
   std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  printf("wait a second again ...\n");
+  std::this_thread::sleep_for(1s);
 }
