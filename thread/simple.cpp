e6e0d7e4aa6d6b2e966571bdadac84d53904bc05
kruztw <kruztw@gmail.com>
2022-01-13 16:32:18 +0800

    update thread

--- thread/simple.cpp
+++ thread/simple.cpp
@@ -7,10 +7,16 @@ void handler()
     std::cout << "hello world\n";
 }
 
+void handler2(int arg1, int arg2)
+{
+    std::cout << "arg1 = " << arg1 << ", arg2 = " << arg2 << std::endl;
+}
+
 int main() 
 {
-  std::thread t1(handler);
+  std::thread t1(handler), t2(handler2, 1, 2);
   t1.join();
+  t2.join();
 
   return 0;
 }
